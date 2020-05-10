d883fe93d3f9a20e08c5659bc9dbde5d6cd44f67
Michael Cook <mcook0775@gmail.com>
2020-05-10 17:52:21 -0400

    Updates

---
+++ precache-manifest.7c4e0c87d4d3ae2bac46d8cb119ce491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b51a8b700922465991c195f33942c73",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "e8e3ca66a2318f8247c9",
+    "url": "/to-do-app/static/css/main.2e2764be.chunk.css"
+  },
+  {
+    "revision": "e2895b9ef23b1a3f0abd",
+    "url": "/to-do-app/static/js/2.a2f774a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.a2f774a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e3ca66a2318f8247c9",
+    "url": "/to-do-app/static/js/main.9241b337.chunk.js"
+  },
+  {
+    "revision": "014f520ead12fae18dcb",
+    "url": "/to-do-app/static/js/runtime-main.5d34271b.js"
+  }
+]);
