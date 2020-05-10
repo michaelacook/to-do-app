630b10abf4477874c14e0af83e63f15e31418557
Michael Cook <mcook0775@gmail.com>
2020-05-10 18:34:51 -0400

    Add data persistence using localStorage

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import Table from './Table'
 import Form from './Form'
 
 class App extends Component {
-
-    state = {
-        items: [],
-        count: 0
+    constructor(props) {
+        super(props);
+        this.state = this.getSavedState();
     }
 
 
@@ -32,14 +31,14 @@ class App extends Component {
                 item.complete = true;
             }
         });
-        this.setState({ items: newItems });
+        this.setState({ items: newItems }, this.saveState);
     }
 
 
     addItem = item => {
         this.setState({ items: [...this.state.items, {item}] });
         const newCount = this.state.count+=1;
-        this.setState({ count: newCount })
+        this.setState({ count: newCount }, this.saveState)
     }
 
 
@@ -48,7 +47,27 @@ class App extends Component {
         const newItems = items.filter((item, i) => i !== index);
         this.setState({ items: newItems });
         const newCount = this.state.count-=1;
-        this.setState({ count: newCount });
+        this.setState({ count: newCount }, this.saveState);
+    }
+
+
+    saveState() {
+        localStorage.clear();
+        const newState = JSON.stringify(this.state);
+        localStorage.setItem('state', newState);
+    }
+
+
+    getSavedState() {
+        const state = localStorage.getItem('state');
+        if (state) {
+            return JSON.parse(state);
+        } else {
+            return {
+                items: [],
+                count: 0
+            }
+        }
     }
 }
 
